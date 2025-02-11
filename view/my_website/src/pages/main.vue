39e0fd7708df4cc8471a3d1fede045bad585b5c2
Chase Lloyd <ctlloyd35@gmail.com>
2025-02-10 19:56:41 -0600

    Updated Landing page with new text

--- view/my_website/src/pages/main.vue
+++ view/my_website/src/pages/main.vue
@@ -10,12 +10,29 @@ export default {
         }
     }
 }
-
-
-
 </script>
 
 
 <template> 
-    <h1>Welcome to the home page!</h1>
+    <div class='calling_card'>
+        <p class='main_tag'>Developer &#183; Designer &#183; Data Analyst</p>
+
+        <p>
+            I am a young developer passionate about creating great user experiences using the best software engineering practices. 
+            My favorite type of work is using my developement skills to create tools, pipelines or other functionality for users.
+        </p>
+    </div>
+
+
 </template> 
+
+<style>
+    .calling_card { 
+        max-width: 600px;  
+        
+    }
+
+    .main_tag { 
+        margin: 0; 
+    }
+</style>
